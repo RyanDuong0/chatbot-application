b8f747e52f88aa890956b0c84d8b2ec9fa37f0d3
Ryan Duong <ryan.duong@aol.co.uk>
2025-06-02 10:56:43 +0100

    Updated pairs for robustness

--- chatbot.py
+++ chatbot.py
@@ -4,12 +4,13 @@ from nltk.chat.util import Chat, reflections
 #list of patterns and responses
 pairs = [
     ["my name is (.*)", ["Hi %1!"]],
-    ["(hi|hello|hey|holla|hola)",["Hey there!", "Hi there!", "Hi!", "Hey!"]],
+    ["(hi|hello|hey|holla|hola)", ["Hey there!", "Hi there!", "Hi!", "Hey!"]],
     ["(.*) in (.*) is fun", ["%1 in %2 is indeed fun"]],
-    ["(.*)(location|city) ?",["London, United Kingdom"]],
-    ["(.*) created you?", ["Ryan did using NLTK!"]]
+    ["(.*)(location|city) ?", ["London, United Kingdom"]],
+    ["(.*) created you?", ["Ryan did using NLTK!"]],
+    # Catch-all pattern
+    ["(.*)", ["Tell me more.", "Interesting...", "Why do you say that?", "I'm listening.", "Let's talk more about that."]]
 ]
-
 #reflections is a dictionary that maps input values to output values (has common responses)
 chat = Chat(pairs, reflections)
 chat.converse()
