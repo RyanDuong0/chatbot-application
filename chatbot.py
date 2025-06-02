048a0c68280b139609d08293136ab4c8569a848e
Ryan Duong <ryan.duong@aol.co.uk>
2025-06-02 10:52:29 +0100

    Add simple feature for chatbot

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+# Description: Chatbot application
+from nltk.chat.util import Chat, reflections
+
+#list of patterns and responses
+pairs = [
+    ["my name is (.*)", ["Hi %1!"]],
+    ["(hi|hello|hey|holla|hola)",["Hey there!", "Hi there!", "Hi!", "Hey!"]],
+    ["(.*) in (.*) is fun", ["%1 in %2 is indeed fun"]],
+    ["(.*)(location|city) ?",["London, United Kingdom"]],
+    ["(.*) created you?", ["Ryan did using NLTK!"]]
+]
+
+#reflections is a dictionary that maps input values to output values (has common responses)
+chat = Chat(pairs, reflections)
+chat.converse()
